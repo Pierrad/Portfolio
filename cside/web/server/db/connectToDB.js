7dd2db5af2bc9cab47b5fda93545bac37f3783c0
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-03-29 10:54:45 +0200

    🔧Update env and docker file

--- cside/web/server/db/connectToDB.js
+++ cside/web/server/db/connectToDB.js
@@ -1,15 +1,15 @@
 const pgp = require('pg-promise')();
 // SSL for Heroku
 let ssl = null;
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'development') {
    ssl = {rejectUnauthorized: false};
 }
 // Get these values from configuration
-const user = process.env.DB_USER
-const password = process.env.DB_PASS
-const host = process.env.DB_HOST
-const port = process.env.DB_PORT || '5432'
-const database = process.env.DB_NAME
+const user = process.env.HEROKU_DB_USER || process.env.DB_USER
+const password = process.env.HEROKU_DB_PASS || process.env.DB_PASS
+const host = process.env.HEROKU_DB_HOST || process.env.DB_HOST
+const port = process.env.HEROKU_DB_PORT|| process.env.DB_PORT || '5432'
+const database = process.env.HEROKU_DB_NAME || process.env.DB_NAME
 
 // Use a symbol to store a global instance of a connection, and to access it from the singleton.
 const DB_KEY = Symbol.for("MyApp.db");
