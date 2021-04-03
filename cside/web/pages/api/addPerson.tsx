ec0dc2ab2f7273d2f3963dde2e72d34fb6cf5f1d
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-04-03 14:35:26 +0200

    :construction: test

--- cside/web/pages/api/addPerson.tsx
+++ cside/web/pages/api/addPerson.tsx
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const db = require('../../server/db/connectToDB').default.instance
 import { NextApiRequest, NextApiResponse } from 'next'
 
-async function call(): Promise<any> {
+async function call() {
   const post = await db.one(
     "INSERT INTO persons (id, firstname, lastname) VALUES (1, 'Isa', 'Lindta')"
   )
   return post
 }
 
-async function handler(req: NextApiRequest, res: NextApiResponse): Promise<any> {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   const result = await call()
   res.status(200).json(result)
 }
